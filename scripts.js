435bdd8c1f0c14baa4edcc6318f73c518a88ca7a
kaiorbm <kakaiorb@gmail.com>
2021-12-13 12:54:51 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,18 @@
+var currentNumberWrapper = document.getElementById('currentNumber')
+var currentNumber = 0
+var = 
+function increment() {
+  currentNumber = currentNumber + 1
+  currentNumberWrapper.innerHTML = currentNumber
+}
+
+function decrement() {
+  currentNumber = currentNumber - 1
+  currentNumberWrapper.innerHTML = currentNumber
+ 
+  
+  
+}
+
+
+
