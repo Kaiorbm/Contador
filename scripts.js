f0f89971c8a9fa25c9fa4b099d3003a8075c784b
Kaio Rodrigues <82424094+Kaiorbm@users.noreply.github.com>
2021-12-13 12:57:18 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 var currentNumberWrapper = document.getElementById('currentNumber')
 var currentNumber = 0
-var = 
+
 function increment() {
   currentNumber = currentNumber + 1
   currentNumberWrapper.innerHTML = currentNumber
